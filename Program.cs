fee4b834d1ce3952aa7eadc6aef3c1371eb23367
Aakankshap16 <aakankshap971@gmail.com>
2023-03-30 22:16:24 +0530

    call display in uc1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("*****Welcome to the Hash Table and Bst problem*****");
+            BinarySearchTree<int> obj = new BinarySearchTree<int>(56);
+            obj.Add(30);
+            obj.Add(70);
+            obj.Display();
         }
     }
 }
